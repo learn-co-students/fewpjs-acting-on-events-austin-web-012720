cced89ead486d6706562c2ca46749061808ac4bc
benbrower <bbrower@smu.edu>
2020-08-17 19:02:08 -0500

    Done.

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ dodger.style.backgroundColor = "#FF69B4";
 dodger.style.left;
 dodger.style.bottom = "0px";
 
-document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
   console.log(e.key);
-  if (e.key === "ArrowLeft"){
+  if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   }
-  if (e.key === "ArrowRight"){
+  if (e.key === "ArrowRight") {
     moveDodgerRight();
   }
 });
 
-function moveDodgerLeft(){
+function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
-  if (left > 0){
+  if (left > 0) {
     dodger.style.left = `${left - 5}px`;
   }
 }
 
-function moveDodgerRight(){
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.left.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
 
-  if (left < 360){
-    dodger.style.left = `${left + 5}px`;
+  if (right < 360) {
+    dodger.style.left = `${right + 5}px`;
   }
 }
